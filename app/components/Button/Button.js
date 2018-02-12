1ce83fedcba9011c4eb5b870af010ceaf4317678
Andrew Salcedo <asalcedo07@gmail.com>
2018-02-12 21:27:15 +0100

    Add PropTypes to <Button>

--- app/components/Button/Button.js
+++ app/components/Button/Button.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
+import PropTypes from 'prop-types'; 
  
 import styles from './styles';
 
 const Button = ({ text, onPress, customIcon = null }) => (
-  <TouchableOpacity
-    onPress={onPress}
-    style={styles.container}
-    activeOpacity={styles.$opacity}
-  >
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.container}
+      activeOpacity={styles.$opacity}
+    >
     <View style={styles.wrapper}>
       {customIcon}
       <Text style={styles.text}>{text}</Text>
     </View>
   </TouchableOpacity>
 );
+
+Button.propTypes = {
+    text: PropTypes.string, 
+    onPress: PropTypes.func,
+    customIcon: PropTypes.object
+}
 
 export default Button;
